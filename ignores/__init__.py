f126c557fb467ecda41fee1006c41d4e3c87c612
jordaniac89 <jordan.k.miles@gmail.com>
2022-05-25 13:50:40 -0400

    cleaning up a function

--- ignores/__init__.py
+++ ignores/__init__.py
@@ -3,7 +3,6 @@ import yaml
 
 
 def retrieve_ignores(org_id: str, project_id: str, api_key: str) -> dict:
-
     request_url = f'https://snyk.io/api/v1/org/{org_id}/project/{project_id}/ignores'
     request_header = {'Authorization': f'token {api_key}'}
 
@@ -17,16 +16,15 @@ def purge_keys(ignores: dict) -> dict:
 
     issues = ignores.keys()
 
-    for issue in issues:
-        ignores_list = ignores[issue]
-        for issue_obj in ignores_list:
-            [issue_obj['*'].pop(key) for key in ['ignoredBy', 'reasonType', 'disregardIfFixable']]
+    [issue_obj['*'].pop(key)
+     for issue in issues
+     for issue_obj in ignores[issue]
+     for key in ['ignoredBy', 'reasonType', 'disregardIfFixable']]
 
     return ignores
 
 
 def ignores_to_snyk(issues_json: dict) -> str:
-
     full_ignore_json = {
         'version': 'v1.25.0',
         'ignore': issues_json}
